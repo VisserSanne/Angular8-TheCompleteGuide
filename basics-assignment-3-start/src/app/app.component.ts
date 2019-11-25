598228a84b2f7b800fea542d7cc7ef9e0db466b2
Sanne <SanneSVisser@gmail.com>
2019-11-25 16:49:05 +0100

    Finished assignment2

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  buttonText = "Display Details";
+  detailsShow = true;
+  buttonClicksNum = 0;
+  buttonClicks = [];
+
+  displayDetails() {
+    this.buttonClicksNum++;
+    this.buttonClicks.push(this.buttonClicksNum);
+    if (this.detailsShow) {
+      this.detailsShow = false;
+      this.buttonText = "Hide details";
+    } else {
+      this.detailsShow = true;
+      this.buttonText = "Display Details";
+    }
+  }
+
+  getColor(click: number) {
+    if (click >= 5) {
+      return "white";
+    }
+  }
+
+  getBackgroundColor(click: number) {
+    if (click >= 5) {
+      return "blue";
+    }
+  }
 }
