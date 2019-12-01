66afd386e41ab1b209a2bae3312ca3046e6c57fb
Sanne <SanneSVisser@gmail.com>
2019-12-01 12:38:45 +0100

    Finished section 3

--- RecipeIngredientsApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ RecipeIngredientsApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Spaghetti", "Pasta with a lovely tomato sauce!", "https://cdn.pixabay.com/photo/2016/06/17/19/10/pasta-1463929_960_720.jpg"),
+    new Recipe("Spaghetti", "Pasta with a lovely tomato sauce!", "https://cdn.pixabay.com/photo/2016/06/17/19/10/pasta-1463929_960_720.jpg")
+  ];
 
   constructor() { }
 
---
+++ RecipeIngredientsApp/src/app/shared/ingredient-model.ts
@@ -0,0 +1,4 @@
+export class Ingredient {
+  // A nice shortcut is that when you add "public" before a value in the constructor it's automatically created as a property
+  constructor(public name: String, public amount: number) {}
+}
