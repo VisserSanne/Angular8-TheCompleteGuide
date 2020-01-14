01b3ab2a4bda0e2307b9ad53259f88d6b465f6d8
Sanne <SanneSVisser@gmail.com>
2020-01-14 13:33:47 +0100

    Finished assignment 6

--- Assignments/forms-td-assignment-start/src/app/app.component.ts
+++ Assignments/forms-td-assignment-start/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  @ViewChild("form", { static: true }) form: NgForm;
+  subscription = "advanced";
+  user = {
+    email: "",
+    subscription: "",
+    password: ""
+  };
+  submitted = false;
 
+  onSubmit() {
+    this.submitted = true;
+    this.user.email = this.form.value.email;
+    this.user.subscription = this.form.value.subscription;
+    this.user.password = this.form.value.password;
+
+    console.log(this.form.value.email);
+    console.log(this.form.value.subscription);
+    console.log(this.form.value.password);
+    this.form.reset();
+  }
 }
