264ca025d4f9ab90f68bec75afc9092622d6a27d
Sanne <SanneSVisser@gmail.com>
2020-01-14 10:04:28 +0100

    Finished section 14

--- RecipeIngredientsApp/src/app/shopping-list/shopping-list.component.ts
+++ RecipeIngredientsApp/src/app/shopping-list/shopping-list.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Ingredient } from "../shared/ingredient-model";
 import { ShoppinglistService } from "./shopping-list.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-shopping-list",
   templateUrl: "./shopping-list.component.html",
   styleUrls: ["./shopping-list.component.css"]
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private igChangeSub: Subscription;
 
   constructor(private shoppinglistService: ShoppinglistService) {}
 
   ngOnInit() {
     this.ingredients = this.shoppinglistService.getIngredients();
-    this.shoppinglistService.ingredientsChanged.subscribe(
+    this.igChangeSub = this.shoppinglistService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
     );
+  }
+
+  ngOnDestroy() {
+    this.igChangeSub.unsubscribe();
   }
 }
--- RecipeIngredientsApp/src/app/shopping-list/shopping-list.service.ts
+++ RecipeIngredientsApp/src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
 import { Ingredient } from "../shared/ingredient-model";
-import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
 
 export class ShoppinglistService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient("Apples", 5),
     new Ingredient("Tomatoes", 5)
@@ -10,7 +10,7 @@ export class ShoppinglistService {
 
   onIngredientAdded(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   onIngredientsAdded(ingredients: Ingredient[]) {
@@ -18,7 +18,7 @@ export class ShoppinglistService {
     //   this.onIngredientAdded(ingredient);
     // });
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   getIngredients() {
